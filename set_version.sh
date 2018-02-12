64dd62eef2b70683d998f1db366630093592ee65
Lusheng Ji <lji@research.att.com>
2018-02-11 23:20:08 -0500

    Update license text
    Change-Id: Ib979013d0f326946b2d8db3fcb893ef15237fd5b
    Signed-off-by: Lusheng Ji <lji@research.att.com>
    Issue-ID: DCAEGEN2-325

--- set_version.sh
+++ set_version.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+# ================================================================================
+# Copyright (c) 2017 AT&T Intellectual Property. All rights reserved.
+# ================================================================================
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============LICENSE_END=========================================================
+#
+# ECOMP is a trademark and service mark of AT&T Intellectual Property.
+
+
 echo "exports.branch=\"$(git branch | grep \* | cut -d ' ' -f2)\";" > version.js
 echo "exports.commit=\"$(git describe --long --always)\";" >> version.js
 echo "exports.commit_datetime=\"$(git show -s --format=%ci)\";" >> version.js
